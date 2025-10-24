44684ad24f2b1b2de65ae00490b1a9a3d35fa24a
thienan216 <pnta2165@gmail.com>
2025-10-24 14:05:51 +0700

    cập nhật tariff và vehicle

--- project_java/src/main/java/ut/edu/evcs/project_java/domain/tariff/TariffPlan.java
+++ project_java/src/main/java/ut/edu/evcs/project_java/domain/tariff/TariffPlan.java
@@ -1,5 +1,19 @@
 package ut.edu.evcs.project_java.domain.tariff;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Table(name = "tariffs")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+
 public class TariffPlan {
-    
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String mode; // PER_KWH, PER_MINUTE, HYBRID, SUBSCRIPTION
+    private double pricePerKWh;
+    private double pricePerMinute;
+    private double idleFeePerMinute;
+    private String timeOfUseRules;
+    private boolean active;
 }
--- project_java/src/main/java/ut/edu/evcs/project_java/domain/vehicle/Vehicle.java
+++ project_java/src/main/java/ut/edu/evcs/project_java/domain/vehicle/Vehicle.java
@@ -1,5 +1,18 @@
 package ut.edu.evcs.project_java.domain.vehicle;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Table(name = "vehicles")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+
 public class Vehicle {
-    
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String driverId; // FK to users.id (EV_DRIVER)
+    private String brand;
+    private String model;
+    private String plateNo;
+    private double batteryCapacityKWh;
+    private String connectorSupported;
 }
