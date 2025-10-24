551b772a0c4740481dca0402fea9471758e167b6
TRANPHANGOCTU0335 <trand3808@gmail.com>
2025-10-24 11:15:43 +0700

    Thêm thư mục config (OpenApiConfig và SecurityConfig)

---
+++ project_java/src/main/java/ut/edu/evcs/project_java/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package ut.edu.evcs.project_java.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class OpenApiConfig {
+@Bean
+public OpenAPI evcsOpenAPI() {
+return new OpenAPI().info(new Info()
+.title("EVCS Monolith API")
+.version("v1")
+.description("API for EV Charging Station Management System"));
+}
+}
