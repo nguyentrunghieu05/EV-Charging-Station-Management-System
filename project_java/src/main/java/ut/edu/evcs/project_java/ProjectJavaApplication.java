03c102fa3d3f1d576316b2d82b861486430982cb
TRANHOANGTHUONG <tht1267@gmail.com>
2025-10-24 08:35:31 +0700

    Cấu hình bộ khung project

---
+++ project_java/src/main/java/ut/edu/evcs/project_java/ProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package ut.edu.evcs.project_java;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// // public class ProjectJavaApplication {
+
+// // 	public static void main(String[] args) {
+// // 		SpringApplication.run(ProjectJavaApplication.class, args);
+// // 	}
+
+// // }
