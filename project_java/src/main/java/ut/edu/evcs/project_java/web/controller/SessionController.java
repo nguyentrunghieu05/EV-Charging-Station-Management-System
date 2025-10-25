4ae7a8df5f09ea49e7707b5bda5cbc7fb431d484
TRANPHANGOCTU0335 <trand3808@gmail.com>
2025-10-25 16:21:41 +0700

    thêm lại style, index

--- project_java/src/main/java/ut/edu/evcs/project_java/web/controller/SessionController.java
+++ project_java/src/main/java/ut/edu/evcs/project_java/web/controller/SessionController.java
@@ -1,20 +1,20 @@
 package ut.edu.evcs.project_java.web.controller;
 
-// import org.springframework.web.bind.annotation.*;
-// import ut.edu.evcs.project_java.domain.session.ChargingSession;
-// import ut.edu.evcs.project_java.service.SessionService;
+import org.springframework.web.bind.annotation.*;
+import ut.edu.evcs.project_java.domain.session.ChargingSession;
+import ut.edu.evcs.project_java.service.SessionService;
 
-// @RestController
-// @RequestMapping("/api/sessions")
-// public class SessionController {
-//     private final SessionService service;
-//     public SessionController(SessionService service) {
-//         this.service = service;
-//     }
+@RestController
+@RequestMapping("/api/sessions")
+public class SessionController {
+    private final SessionService service;
+    public SessionController(SessionService service) {
+        this.service = service;
+    }
 
-//     @PostMapping("/start")
-//     public ChargingSession start(@RequestBody ChargingSession payload) {
-//         payload.setStatus("STARTED");
-//         return service.start(payload);
-//     }
-// }
+@PostMapping("/start")
+    public ChargingSession start(@RequestBody ChargingSession payload) {
+        payload.setStatus("STARTED");
+        return service.start(payload);
+    }
+}
