3a7329ced49c3420aa3f23a26da58dddc81e7d23
nguyentrunghieu05 <hieule6k123@gmail.com>
2025-11-02 18:53:06 +0700

    update station

--- project_java/src/main/java/ut/edu/evcs/project_java/domain/Station.java
+++ project_java/src/main/java/ut/edu/evcs/project_java/domain/Station.java
@@ -1,17 +1,9 @@
 package ut.edu.evcs.project_java.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "stations")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Station {
 
     @Id
@@ -31,4 +23,124 @@ public class Station {
 
     @Column(name = "available_ports", nullable = false)
     private int availablePorts;
+
+    public Station() {
+    }
+
+    public Station(Long id, String name, String address, double lat, double lng, int availablePorts) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.lat = lat;
+        this.lng = lng;
+        this.availablePorts = availablePorts;
+    }
+
+    public static StationBuilder builder() {
+        return new StationBuilder();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public int getAvailablePorts() {
+        return availablePorts;
+    }
+
+    public void setAvailablePorts(int availablePorts) {
+        this.availablePorts = availablePorts;
+    }
+
+    public static class StationBuilder {
+        private Long id;
+        private String name;
+        private String address;
+        private double lat;
+        private double lng;
+        private int availablePorts;
+
+        StationBuilder() {
+        }
+
+        public StationBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public StationBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public StationBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public StationBuilder lat(double lat) {
+            this.lat = lat;
+            return this;
+        }
+
+        public StationBuilder lng(double lng) {
+            this.lng = lng;
+            return this;
+        }
+
+        public StationBuilder availablePorts(int availablePorts) {
+            this.availablePorts = availablePorts;
+            return this;
+        }
+
+        public Station build() {
+            return new Station(id, name, address, lat, lng, availablePorts);
+        }
+
+        @Override
+        public String toString() {
+            return "Station.StationBuilder(id=" + id + ", name=" + name + ", address=" + address + ", lat=" + lat + ", lng=" + lng + ", availablePorts=" + availablePorts + ")";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Station(id=" + id + ", name=" + name + ", address=" + address + ", lat=" + lat + ", lng=" + lng + ", availablePorts=" + availablePorts + ")";
+    }
 }
